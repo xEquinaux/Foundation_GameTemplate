8bb6e2c05d247d7219e363bb690930bae5b8a2a4
Nolan Flammini <nolantheturtle@gmail.com>
2024-01-23 20:56:15 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FoundationR;
+
+namespace CastleOfTheFlame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CastleOfTheFlame.Main m = null;
+            Thread t = new Thread(() => { (m = new Main()).Run(new Surface(0, 0, 640, 480, "Castle of the Flame")); });
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+            while (Console.ReadLine() != "exit");
+        }
+    }
+    public class Main : Foundation
+    {
+        internal Main()
+        {
+        }
+        public override void Update()
+        {
+        }
+        public override void Draw(Graphics graphics)
+        {
+        }
+    }
+}
