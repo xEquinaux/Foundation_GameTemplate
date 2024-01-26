cc5b6bcf2273dc2e7bc23d3a08f8a9c021b9bc99
Nolan Flammini <nolantheturtle@gmail.com>
2024-01-26 14:08:32 -0800

    [unstable] error manually creating the DIB header for 32 bpp

--- Program.cs
+++ Program.cs
@@ -38,7 +38,7 @@ namespace CastleOfTheFlame
         }
         public override void LoadResources()
         {
-            image = ImageLoader.BitmapIngest(new BitmapFile() { Value = (Bitmap)Bitmap.FromFile(@"C:\Users\nolan\Pictures\frame_splashed_by_nolantheturtle_d2u6wkk-fullview.jpg") });
+            image = ImageLoader.BitmapIngest(new BitmapFile() { Value = (Bitmap)Bitmap.FromFile(@"C:\Users\nolan\source\repos\test_env\Textures\bluepane.png") }, PixelFormats.Bgr32);
             Lib.SetDimensions(640, 480);
             Lib.Initialize(8, new Size(20, 20));
             Texture.GenerateColorTextureFiles(Tile.Instance.TexturePrefix, Color.Gray, new Size(20, 20));
@@ -51,7 +51,7 @@ namespace CastleOfTheFlame
         public override void Draw(RewBatch rewBatch)
         {
             if (image != null)
-            { 
+            {
                 rewBatch.Draw(image, 0, 0);
             }
         }
