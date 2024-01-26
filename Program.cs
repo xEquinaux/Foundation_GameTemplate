c69410f0dffb99e6b143b7a717863ddf537fc532
700hours <31488826+700hours@users.noreply.github.com>
2024-01-25 21:14:33 -0800

    nightly update

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using cotf;
 using FoundationR;
 
 namespace CastleOfTheFlame
@@ -18,18 +19,36 @@ namespace CastleOfTheFlame
             t.SetApartmentState(ApartmentState.STA);
             t.Start();
             while (Console.ReadLine() != "exit");
+            t.Abort();
+            Environment.Exit(0);
         }
     }
     public class Main : Foundation
     {
+        bool init = false;
+        REW image;
         internal Main()
         {
+        }
+        public override void Initialize()
+        {
+            Lamp.NewLamp(80, 80, 200, Color.Orange, true);
+        }
+        public override void LoadResources()
+        {
+            image = REW.Create(40, 60, Color.White);
+            Lib.SetDimensions(640, 480);
+            Lib.Initialize(8, new Size(20, 20));
+            Texture.GenerateColorTextureFiles(Tile.Instance.TexturePrefix, Color.Gray, new Size(20, 20));
+            Texture.GenerateColorTextureFiles(cotf.Background.Instance.TexturePrefix, Color.DarkGray, new Size(20, 20));
+            Lib.InitArray();
         }
         public override void Update()
         {
         }
-        public override void Draw(Graphics graphics)
+        public override void Draw(RewBatch rewBatch)
         {
+            rewBatch.Draw(image, 20, 20);
         }
     }
 }
